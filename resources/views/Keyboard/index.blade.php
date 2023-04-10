6f3d32a2ed7448f18456147fb984a7bb1be6cd72
Scorpoll23 <oliverthompsonuni@gmail.com>
2023-04-10 23:57:44 +0100

    first commit

---
+++ resources/views/Keyboard/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>Keyboard</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <script src="https://unpkg.com/tone"></script>
+    <script src="{{ asset('js/qwerty-hancock.min.js') }}"></script>
+</head>
+<body>
+    <h1>Keyboard Of Truth</h1>
+    <div id="keyboard" class="center"></div>
+    <script>
+        // Initialize Tone.js
+        Tone.start();
+
+        // Create an array of the different instrument types
+        const instruments = [
+            {
+                type: 'Please',
+                notesPath: 'sounds/Please/',
+            },
+            {
+                type: 'Hire',
+                notesPath: 'sounds/Hire/',
+            },
+            {
+                type: 'Me',
+                notesPath: 'sounds/Me/',
+            },
+        ];
+
+        // Initialize the current instrument index
+        let currentInstrumentIndex = 0;
+
+        // Create a function to get the current instrument object
+        function getCurrentInstrument() {
+            return instruments[currentInstrumentIndex];
+        }
+
+        // Create a function to cycle to the next instrument
+        function cycleInstrument() {
+            currentInstrumentIndex = (currentInstrumentIndex + 1) % instruments.length;
+        }
+
+        // Create audio element from note and instrument value
+        function playSound(note, instrument) {
+            const sound = new Audio(`${instrument.notesPath}${note}.wav`);
+            sound.play();
+        }
+        
+
+        // create the keyboard UI
+        var keyboardUI = new QwertyHancock({
+            id: 'keyboard',
+            width: 600,
+            height: 150,
+            octaves: 1,
+            startNote: 'C4',
+            whiteNotesColour: 'white',
+            blackNotesColour: 'black',
+            hoverColour: '#f3e939'
+        });
+
+
+        // bind keyboard events to trigger the sampler
+        keyboardUI.keyDown = function(note, frequency) {
+            instrument = getCurrentInstrument(); 
+            playSound(note, instrument);
+        }
+        keyboardUI.keyUp = function(note, frequency) {
+            cycleInstrument();
+            console.log(`Current instrument: ${getCurrentInstrument().type}`);
+        }
+
+    </script>
+</body>
+</html>
